16ba8a1e46eb92112c7cb86e5f1705c5380f6eb9
hynookim98 <hynookim98@gmail.com>
2022-12-31 12:41:38 -0800

    added sequelize connection and started index.js in models

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});

4e89fe5e7fb5808c8e7503551f7183fe62f4efe1
hynookim98 <hynookim98@gmail.com>
2023-01-05 13:36:07 -0800

    added schema.sql and finished main routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -8,6 +8,38 @@ router.get('/', auth, async (req, res) => {
 
         const posts = postData.map((data) => data.get({plain: true}));
 
-        res.render('all-posts-admin', {})
+        res.render('all-posts-admin' {
+            layout: 'dashboard',
+            posts,
+        });
+    } catch(err) {
+        res.redirect('login');
     }
-})
+});
+
+router.get('/new', auth, async (req, res) => {
+    res.render('new-post', {
+        layout: 'dashboard',
+    });
+});
+
+router.get('/edit/:id', auth, async (req, res) => {
+    try{
+        const postData = await Post.findByPk(req.params.id);
+        if(postData) {
+            const post = postData.get({ plain: true });
+            res.render('edit-post', {
+                layout: 'dashboard',
+                post,
+            });
+            // if id doesn't exist end()
+        } else {
+            res.status(404).end();
+        }
+    } catch(err) {
+        // if user is not logged in or if logged in session timed out
+        res.redirect('login');
+    }
+});
+
+module.exports = router;
